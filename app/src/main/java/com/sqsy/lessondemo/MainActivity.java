3bbd6c0af48ff9e1c8e8c563adbb357c294fedf5
AssIstne <ljiaquan929@gmail.com>
2023-05-29 17:35:17 +0800

    a-初始化android

---
+++ app/src/main/java/com/sqsy/lessondemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sqsy.lessondemo;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author liangjiaquan
+ * @since 2023/5/29
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
